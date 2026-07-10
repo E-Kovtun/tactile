c74bbb654ec307d7b90e2d4012e8d41fa5349d72
Matvey Konovalov <konovalov.matvej@mail.ru>
2026-07-10 10:22:43 +0300

    Поправил скрипт

--- scripts/dinov2_orig_downstreams.sh
+++ scripts/dinov2_orig_downstreams.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-cd "${ROOT}"
 
-python train.py \
+python train_task_force.py \
   +experiment=xela/task/force/dinov2 \
   experiment_name=downstream_force_dinov2 \
   run_name=Original_DiNO_fixed \
   task.checkpoint_encoder=/workspace-SR004.nfs2/konovalov/tactile/experiments/pretrain_dinov2_encoder_dinov2/2026.07.09_17-26/checkpoints/epoch-0500.ckpt
   
-python train.py \
+python train_task_object.py \
   +experiment=xela/task/object_classification/dinov2 \
   experiment_name=downstream_object_dinov2 \
   run_name=Original_DiNO_fixed \
   task.checkpoint_encoder=/workspace-SR004.nfs2/konovalov/tactile/experiments/pretrain_dinov2_encoder_dinov2/2026.07.09_17-26/checkpoints/epoch-0500.ckpt
   
-python train.py \
+python train_task_pose_estimation.py \
   +experiment=xela/task/relative_pose_estimation/dinov2 \
   experiment_name=downstream_pose_dinov2 \
   run_name=Original_DiNO_fixed \
